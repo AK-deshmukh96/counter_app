1adda36802b9c9afa2c1ff3d8873a7719a2c3494
AK-deshmukh96 <akawade777@gmail.com>
2023-12-27 22:30:45 +0530

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import './Counter.css'; // Styling file
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="counter-container">
+      <h2>Counter Value: {count}</h2>
+      <div className="button-container">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
